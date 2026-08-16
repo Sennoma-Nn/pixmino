4e78cd6df413ce56ff700c79157630f09472a246
Sennoma-Nn <3772564985@qq.com>
2026-08-16 19:44:11 +0800

    update version

--- src/utils/locale.lua
+++ src/utils/locale.lua
@@ -7,7 +7,7 @@ local game_debug = require("src.game.debug")
 locale.langs = { "en", "ja", "zh_cn", "zh_tw" }
 locale.current = "en"
 
-local ver = "v0.0.13"
+local ver = "v0.0.14"
 
 local features = game_debug.detect_features()
 
